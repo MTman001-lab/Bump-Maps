2098e81d1378c9b0997ee002baba301f6a4bfe6f
Joshua <joshuamuhorondirangu@gmail.com>
2025-09-29 11:59:50 +0300

    Joshua Updated

--- bump_ingestion_service/bump-ingestion-service/src/db.py
+++ bump_ingestion_service/bump-ingestion-service/src/db.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, Column, String, Float, Integer, Enum, TIME
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import enum
+from config import DB_URL
 
 Base = declarative_base()
 
